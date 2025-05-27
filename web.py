26299b48a021d3d7b91f0455d7bfe9c25a94359b
Njlr41 <nicholasjohnreyes@gmail.com>
2025-05-27 08:06:13 +0800

    fix: model being passed incorrectly

--- web.py
+++ web.py
@@ -95,6 +95,7 @@ tokenizer = AutoTokenizer.from_pretrained("njlr/cs180-project")
 nb_model = pickle.load(open("./models/trad_model.sav", "rb"))
 lr_model = pickle.load(open("./models/lr_model.sav", "rb"))
 bert_model = AutoModelForSequenceClassification.from_pretrained("njlr/cs180-project")
+bert_model.eval()
 
 st.title("🍃 Climate Sentiment Analysis")
 
